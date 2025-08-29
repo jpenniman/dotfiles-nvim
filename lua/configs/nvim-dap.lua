c3cf06ad5c373a71b7e518d6fe0c62b4470c6d88
ramboe <yourmom@ramboe.io>
2025-08-29 11:22:02 +0200

    refactor debugging ui

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -37,36 +37,3 @@ map("n", "<leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>", opts)
 map("n", "<leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
 map("n", "<leader>dt", "<Cmd>lua require('neotest').run.run({strategy = 'dap'})<CR>",
   { noremap = true, silent = true, desc = 'debug nearest test' })
-
--- local dapui = require("dapui")
---
--- dapui.setup({
---   expand_lines = true,
---   controls = { enabled = false }, -- no extra play/step buttons
---   floating = { border = "rounded" },
---   render = {
---     max_type_length = 60,
---     max_value_lines = 200,
---   },
---   -- Only one layout: just the "scopes" (variables) list at the bottom
---   layouts = {
---     {
---       elements = {
---         { id = "scopes", size = 1.0 }, -- 100% of this panel is scopes
---       },
---       size = 20,                       -- height in lines (adjust to taste)
---       position = "bottom",             -- "left", "right", "top", "bottom"
---     },
---   },
--- })
---
--- -- Auto open/close UI with sessions
--- dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
--- -- dap.listeners.after.event_initialized["dapui_config"] = function() dapui.float_element("scopes", {}) end
---
--- dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
--- dap.listeners.before.event_exited["dapui_config"]     = function() dapui.close() end
---
--- -- Optional: a simple toggle key if you want manual control sometimes
--- vim.keymap.set("n", "<leader>du", function() dapui.toggle() end,
---   { noremap = true, silent = true, desc = "Toggle DAP UI" })
