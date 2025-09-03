6942d931e9904c76dcdf6493a667bff4885897b1
ramboe <yourmom@ramboe.io>
2025-09-03 16:56:40 +0200

    cleanups

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,38 +6,6 @@ local M = {}
 
 
 M.base46 = {
-  -- theme = "nightowl",
-  -- theme = "aquarium",
-  -- theme = "mountain",
-  -- theme = "falcon-custom",
-
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
-
-  -- hl_override = {
-  --   syntax = {
-  --     Type = { bold = true, italic = true },
-  --   },
-  --
-  --   --- https://neovim.io/doc/user/treesitter.html#treesitter-highlight-groups
-  --   treesitter = {
-  --     ["@function"] = { bold = true, italic = true },
-  --     ["@function.builtin"] = { bold = true },
-  --     -- ["@function.call"] = { bold = true, fg = M.base_30.dark_purple },
-  --     ["@function.call"] = { bold = true },
-  --     -- ["@constructor"] = { fg = M.base_30.purple },
-  --     -- ["@variable.parameter"] = { fg = M.base_30.white },
-  --     -- ["@module"] = { fg = M.base_30.deep_black },
-  --     -- ["@symbol"] = { fg = M.base_30.purple },
-  --     ["@keyword"] = { italic = true },
-  --     ["@function.method.call"] = { bold = true },
-  --     -- ["@comment"] = { italic = true, bg = M.base_30.baby_pink },
-  --     -- ["@comment"] = { bold = true, italic = true, bg = M.base_30.red },
-  --     -- ["@keyword"] = { fg = M.base_16.base0D },
-  --   },
-  -- }
   hl_override = {
     Type = { bold = true, italic = false },
     ["@comment"] = { italic = true },
@@ -51,20 +19,5 @@ M.base46 = {
   }
 }
 
--- M.ui = {
---   base46 = {
---     hl_override = {
---       Type = { bold = true, italic = false },
---       ["@comment"] = { italic = true },
---
---       --- https://neovim.io/doc/user/treesitter.html#treesitter-highlight-groups
---       ["@function"] = { bold = true, italic = true },
---       ["@function.builtin"] = { bold = true },
---       ["@function.call"] = { bold = true },
---       ["@keyword"] = { italic = true },
---       ["@function.method.call"] = { bold = true }
---     }
---   }
--- }
 
 return M
