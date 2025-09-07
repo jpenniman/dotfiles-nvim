73f8ac0da53715d440faaf72329c3d36ecd3bb09
ramboe <yourmom@ramboe.io>
2025-09-07 11:53:29 +0200

    cleanups so far

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -187,15 +187,6 @@ return {
     end,
   },
   {
-    "kaiwalter/azure-functions.nvim",
-    config = function()
-      require("azure-functions").setup {
-        compress_log = true,
-      }
-    end,
-    lazy = false,
-  },
-  {
     'stevearc/oil.nvim',
     opts = {},
     -- Optional dependencies
@@ -212,51 +203,6 @@ return {
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" }
   },
-
-  -- integrate into local ai
-  {
-    "David-Kunz/gen.nvim",
-    lazy = false,
-    opts = {
-      apisecret =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkMzBlNDllLWEzNGMtNDY1Yy04OTYzLTNiNTQ1MGM3N2E0OSJ9.rOuX3yIwioVWtwXQYlxF-DQ-LbLvkY2uTjQaKdzjr6w",
-      model = "aya-expanse:8b", -- The default model to use.
-      -- model = "deepseek-coder-v2", -- The default model to use.
-      quit_map = "q",           -- set keymap for close the response window
-      retry_map = "<c-r>",      -- set keymap to re-send the current prompt
-      accept_map = "<c-cr>",    -- set keymap to replace the previous selection with the last result
-
-      -- host = "owebui.ramboe.io",          -- The host running the Ollama service.
-      -- port = "443",              -- The port on which the Ollama service is listening.
-
-      host = "localhost",     -- The host running the Ollama service.
-      port = "3000",          -- The port on which the Ollama service is listening.
-      display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-      show_prompt = true,     -- Shows the prompt submitted to Ollama.
-      show_model = true,      -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false,  -- Never closes the window automatically.
-      hidden = false,         -- Hide the generation window (if true, will implicitly set `prompt.replace = true`)
-      -- init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-      -- Function to initialize Ollama
-      command = function(options)
-        local body = { model = options.model, stream = true }
-
-        return "curl --silent --no-buffer -X POST http://" ..
-            options.host ..
-            ":" ..
-            options.port ..
-            "/api/chat/completions -H 'Authorization: Bearer " ..
-            options.apisecret .. "' -H 'Content-Type: application/json' -d $body"
-      end,
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a command string.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-      debug = false -- Prints errors and the command which is run.
-    }
-  },
-
   {
     "nvim-neotest/neotest",
     commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
