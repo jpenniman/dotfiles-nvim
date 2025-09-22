9099d447e71277e8e4beb753285ee13b7e9dac6e
ramboe <yourmom@ramboe.io>
2025-09-22 09:07:00 +0200

    migrate from require('lspconfig') to vim.lsp.config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,93 +1,31 @@
 -- !!! https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+require("nvchad.configs.lspconfig").defaults()
 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "ansiblels", "eslint", "jsonls", "ts_ls","yamlls" }
+local servers = { "html", "cssls", "ansiblels", "eslint", "jsonls", "ts_ls", "yamlls" }
 
 -- lsps with default config
 require("nvchad.configs.lspconfig").defaults()
 vim.lsp.enable(servers)
 
-lspconfig.vls.setup {}
-
--- vim.lsp.config('azure_pipelines_ls', {
---   -- other configuration
---   settings = {
---       yaml = {
---           schemas = {
---               ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
---                   -- "/azure-pipeline*.y*l",
---                   "/azure-cli*.y*l",
---                   "/*.azure*",
---                   "Azure-Pipelines/**/*.y*l",
---                   "Pipelines/*.y*l",
---               },
---           },
---       },
---   },
--- })
-
--- vim.lsp.enable('azure_pipelines_ls')
-
--- bicep LSP
+-- keep filetype detection
 vim.cmd([[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]])
 
+-- path to the DLL
 local bicep_lsp_bin =
 "/home/ramboe/.local/share/nvim/mason/packages/bicep-lsp/extension/bicepLanguageServer/Bicep.LangServer.dll"
 
-lspconfig.bicep.setup({
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+vim.lsp.config("bicep", {
   cmd = { "dotnet", bicep_lsp_bin },
-  filetypes = { "bicep" }
+  filetypes = { "bicep" },
 })
--- END bicep LSP
 
-lspconfig.dockerls.setup({
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  filetypes = { "Dockerfile" }
-})
+vim.lsp.enable("bicep")
+
+vim.lsp.config("dockerls", {})
+vim.lsp.enable("dockerls")
 
 vim.lsp.config("roslyn", {})
 
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  -- on_init = on_init,
-  capabilities = capabilities,
-  cmd = { "rustup", "run", "stable", "rust-analyzer" },
-})
-
--- Hyprlang LSP
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  pattern = { "*.hl", "hypr*.conf" },
-  callback = function(event)
-    print(string.format("starting hyprls for %s", vim.inspect(event)))
-    vim.lsp.start {
-      name = "hyprlang",
-      cmd = { "hyprls" },
-      root_dir = vim.fn.getcwd(),
-    }
-  end
-})
-
--- lspconfig.eslint.setup({
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
---   --- ...
---   -- on_attach = function(client, bufnr)
---   --   vim.api.nvim_create_autocmd("BufWritePre", {
---   --     buffer = bufnr,
---   --     command = "EslintFixAll",
---   --   })
---   -- end,
--- })
 
 -- IMPORTANT: vim diagnostic configuration AFTER LSPs are loaded
 vim.diagnostic.config(
