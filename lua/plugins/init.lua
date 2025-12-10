46db66df1fc0a5450ac7cd660e6ff1e6030f0b1a
ramboe <yourmom@ramboe.io>
2025-12-10 11:14:56 +0100

    final cleanups

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,25 +9,6 @@ return {
     ---@type RoslynNvimConfig
     ft = { "cs", "razor" },
     lazy = false,
-    --  https://github.com/tris203/rzls.nvim?tab=readme-ov-file#composing-the-command-for-roslyn
-    config = function()
-      local mason_root = require("mason.settings").current.install_root_dir
-      local rzls_path = vim.fn.expand(mason_root .. "/packages/roslyn/libexec/.razorExtension")
-
-      local cmd = {
-        "roslyn",
-        "--stdio",
-        "--logLevel=Information",
-        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-        "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-        "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-        "--extension=" .. vim.fs.joinpath(rzls_path, "Microsoft.VisualStudioCode.RazorExtension.dll"),
-      }
-
-      vim.lsp.config("roslyn", {
-        cmd = cmd,
-      })
-    end
   },
   {
     "ibhagwan/fzf-lua",
